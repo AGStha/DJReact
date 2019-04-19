d73c210a6538f14554289c1194eeab32c53b520e
AGStha <gongmakz.anuj@gmail.com>
2019-04-19 14:46:44 +0545

    final commit

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -2,11 +2,13 @@ import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+
 export class Alerts extends Component {
-  static PropTypes = {
+  static propTypes = {
     error: PropTypes.object.isRequired,
     message: PropTypes.object.isRequired
   };
+
   componentDidUpdate(prevProps) {
     const { error, alert, message } = this.props;
     if (error !== prevProps.error) {
@@ -14,22 +16,26 @@ export class Alerts extends Component {
       if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
       if (error.msg.message)
         alert.error(`Message: ${error.msg.message.join()}`);
-
-      if (message != prevProps.message) {
-        if (message.deleteLead) alert.success(message.deleteLead);
-        if (message.addLead);
-      }
+      if (error.msg.non_field_errors)
+        alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
     }
-    //this.props.alert.show("hello it works");
+
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
+    }
   }
+
   render() {
     return <Fragment />;
   }
 }
+
 const mapStateToProps = state => ({
   error: state.errors,
   message: state.messages
 });
 
-export default connect(mapStateToProps)(withAlert)(Alerts);
-//export default connect(mapStateToProps)(withAlert()(Alerts));
+export default connect(mapStateToProps)(withAlert(Alerts));
--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -2,11 +2,19 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
-#Lead viewset
-class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
-    permission_classes = [
-        permissions.AllowAny
+# Lead viewset
 
-    ] 
+
+class LeadViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated
+
+    ]
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
